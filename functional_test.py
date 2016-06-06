efb98f9bec9d705a8e0c2b1eadf8d6a975e19dd7
Arjan Geers <ajgeers@gmail.com>
2016-06-06 22:16:55 +0100

    Spec out FT in comments and start using unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # User goes to homepage.
+        self.browser.get('http://localhost:8000')
+
+        # Page title mentions to-do lists.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # User is immediately invited to enter a to-do item.
+
+        # User types "Buy peacock feathers".
+
+        # When user hits enter, the page updates, and now the page lists
+        # "1. Buy peacock feather" as an item in a to-do list.
+
+        # There is still a box inviting the user to add another item. She
+        # enters "Use peacock feather to make a fly".
+
+        # The page updates again, and now shows both items on her list.
+
+        # The site has generated a unique URL for the user -- there is some
+        # text to explain this.
+
+        # By visiting this unique URL, the user has access to her to-do list
+        # again.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
