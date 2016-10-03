d37cc36bbafa279f4f1e53d67d81cfe63bb566ab
Arjan Geers <ajgeers@gmail.com>
2016-10-03 21:43:32 +0100

    Fix spelling of attribute

--- functional_tests.py
+++ functional_tests.py
@@ -25,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
         # User is invited to enter a to-do item straight away.
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attributes('placeholder'),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
