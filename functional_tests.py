2c5cfdcc48a490a3b98379839786a1a2c9769b48
Arjan Geers <ajgeers@gmail.com>
2017-01-17 20:17:18 +0000

    Add helper method to check for a row in a list table

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # User goes to homepage.
@@ -33,27 +38,20 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         # When user hits enter, the page updates, and now the page lists
-        # "1. Buy peacock feather" as an item in a to-do list.
+        # "1. Buy peacock feather" as an item in a to-do list table.
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a box inviting the user to add another item. The user
-        # enters "Use peacock feather to make a fly".
+        # enters "Use peacock feathers to make a fly".
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feather to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make ' +
+                                         'a fly')
 
         # The site has generated a unique URL for the user -- there is some
         # text to explain this.
