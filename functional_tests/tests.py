07fcc1cdce454a935ea09695be2dd30145c10c8d
Arjan Geers <ajgeers@gmail.com>
2017-01-23 21:29:26 +0000

    Extend user story in functional test to include unique URL for lists and a second user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        # User goes to homepage.
+        # Alice goes to homepage.
         self.browser.get(self.live_server_url)
 
         # Page title and header mention to-do lists.
@@ -26,22 +26,24 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # User is invited to enter a to-do item straight away.
+        # Alice is invited to enter a to-do item straight away.
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # User types "Buy peacock feathers" into a text box.
+        # Alice types "Buy peacock feathers" into a text box.
         inputbox.send_keys('Buy peacock feathers')
 
-        # When user hits enter, the page updates, and now the page lists
+        # When Alice hits enter, the page updates, and now the page lists
         # "1. Buy peacock feather" as an item in a to-do list table.
         inputbox.send_keys(Keys.ENTER)
+        alice_list_url = self.browser.current_url
+        self.assertRegex(alice_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # There is still a box inviting the user to add another item. The user
+        # There is still a box inviting Alice to add another item. The user
         # enters "Use peacock feathers to make a fly".
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
@@ -52,9 +54,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('2: Use peacock feathers to make ' +
                                          'a fly')
 
-        # The site has generated a unique URL for the user -- there is some
-        # text to explain this.
-        self.fail('Finish the test!')
+        # A new user, Bob, vists the site. We use a new browser session to make
+        # sure that no information of Alice's is coming through from cookies
+        # etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # By visiting this unique URL, the user has access to her to-do list
-        # again.
+        # Bob visits the home page. There is no sign of Alice's list.
+        self.browser.get(self.live_server_url)
+        page_text = self.broser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Bob starts a new list by entering a new item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Bob gets his own unique URL.
+        bob_list_url = self.browser.current_url
+        self.assertRegex(bob_list_url, '/lists/.+')
+        self.assertNotEqual(bob_list_url, alice_list_url)
+
+        # Again, there is no trace of Alice's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
